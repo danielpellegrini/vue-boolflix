3ccb28579830b6215050a351f0773effc4a64685
Daniel <danielpellegrini87@gmail.com>
2021-02-10 18:17:39 +0100

    Working on Milestone 4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
-
 
 var app = new Vue({
   el: '#app',
@@ -14,57 +6,69 @@ var app = new Vue({
     movies: [],
     tvSeries: [],
     selected: '',
-    flags: []
+    flags: [],
+    //images
+    posterUrl: 'https://image.tmdb.org/t/p/w342'
 
   },
 
   methods: {
 
     getMovies: function() {
-      const self = this;
 
       axios
-      .get('https://api.themoviedb.org/3/search/movie?api_key=1a20e46ffbcbc0628119863e1c4600e8&query=' + self.selected)
-      .then(function(resp) {
-        const movie = resp.data.results;
-        console.log(movie);
-        self.movies = movie;
-
-        self.movies.forEach(function(item) {
-          if(!self.flags.includes(item.original_language)) {
-            self.flags.push(item.original_language)
+        .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: '1a20e46ffbcbc0628119863e1c4600e8',
+            query: this.selected
           }
         })
+        .then((resp) => {
+          this.movies = resp.data.results;
 
-      });
+          // pushes languages into the flags array, without duplicating them
+          this.movies.forEach((item) => {
+            if (!this.flags.includes(item.original_language)) {
+              this.flags.push(item.original_language)
+            }
+          });
+
+        });
     },
     getTvSeries: function() {
-      const self = this;
 
       axios
-      .get('https://api.themoviedb.org/3/search/tv?api_key=1a20e46ffbcbc0628119863e1c4600e8&query=' + self.selected)
-      .then(function(resp) {
-        const tvSerie = resp.data.results;
-        console.log(tvSerie);
-        self.tvSeries = tvSerie;
-
-        self.tvSeries.forEach(function(item) {
-          if(!self.flags.includes(item.original_language)) {
-            self.flags.push(item.original_language)
+        .get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: '1a20e46ffbcbc0628119863e1c4600e8',
+            query: this.selected
           }
         })
+        .then((resp) => {
+          this.tvSeries = resp.data.results;
 
-      });
+          // pushes languages into the flags array, without duplicating them
+          this.tvSeries.forEach((item) => {
+            if (!this.flags.includes(item.original_language)) {
+              this.flags.push(item.original_language)
+            }
+          });
+
+        });
     },
     voteAverage: function(item) {
       return Math.round(item / 2);
     },
     search: function() {
+
       if (this.selected !== '') {
         this.getMovies();
         this.getTvSeries();
       }
     }
+
+  },
+  mounted() {
 
   }
 
