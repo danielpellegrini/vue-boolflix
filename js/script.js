3141956ee237bfe0709af6f8792e9b9c182e5270
Daniel <danielpellegrini87@gmail.com>
2021-05-03 14:00:10 +0200

    top 20 movies and TV shows added to home

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 var app = new Vue({
   el: '#app',
   data: {
@@ -6,6 +5,9 @@ var app = new Vue({
 
     movies: [],
     tvSeries: [],
+    trendingMovies: [],
+    trendingTv: [],
+    genres: [],
     selected: '',
     flags: [],
     //images
@@ -13,24 +15,22 @@ var app = new Vue({
     // dynamic classes
     toggledSearch: false,
     toggledMenu: false,
-
-
-
-
+    toggledTrending: false,
+    toggledNoContent: true
   },
 
 
   methods: {
 
-    getMovies: function() {
+    getMovies() {
 
-      axios
-        .get('https://api.themoviedb.org/3/search/movie', {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: this.api_key,
             query: this.selected
           }
         })
+
         .then(resp => {
 
           this.movies = resp.data.results;
@@ -44,6 +44,7 @@ var app = new Vue({
               this.flags.push(item.original_language)
             }
           });
+        
 
           // gets the firsts 5 cast names
           this.movies.forEach(item => {
@@ -51,49 +52,25 @@ var app = new Vue({
             this.movies = []; // emptying array, otherwise it would duplicate movies
 
             let cast = [];
-            axios
-              .get(`https://api.themoviedb.org/3/movie/${item.id}/credits?`, {
-                 params: {
-                    api_key: this.api_key
-                 }
-               })
-               .then(resp => {
-                 resp.data.cast.slice(0, 5).forEach(item => {
-                   cast.push(item.name);
-                 });
-                 item.cast = cast.toString();
-                 this.movies.push(item);  // pushing cast key into movies array
-               })
+            axios.get(`https://api.themoviedb.org/3/movie/${item.id}/credits?`, {
+                params: {
+                  api_key: this.api_key
+                }
+              })
+              .then(resp => {
+                resp.data.cast.slice(0, 5).forEach(item => {
+                  cast.push(item.name);
+                });
+                item.cast = cast.toString();
+                this.movies.push(item); // pushing cast key into movies array
+              })
           });
 
-          //gets genres ----------------------- NOT WORKING---------------------------------------------------
-          this.movies.forEach(item => {
-
-            this.movies = [];
-
-            let genre = [];
-            axios
-              .get('https://api.themoviedb.org/3/genre/movie/list?', {
-                 params: {
-                    api_key: this.api_key
-                 }
-               })
-               .then(resp => {
-                 resp.data.genres.forEach(item => {
-                   genre.push(item.name);
-                 });
-                 // item.genre = genre.toString();
-                 this.movies.push(item);
-
-               })
-
-          });
-
-
-        });
-
+        })
+        
     },
-    getTvSeries: function() {
+
+    getTvSeries () {
 
       axios
         .get('https://api.themoviedb.org/3/search/tv', {
@@ -123,40 +100,162 @@ var app = new Vue({
             let cast = [];
             axios
               .get(`https://api.themoviedb.org/3/movie/${item.id}/credits?`, {
-                 params: {
-                    api_key: this.api_key
-                 }
-               })
-               .then(resp =>{
-                 resp.data.cast.slice(0, 5).forEach(item => {
-                   cast.push(item.name);
-                 });
-                 item.cast = cast.toString();
-                 this.tvSeries.push(item);  // pushing cast key into tvSeries array
-               })
+                params: {
+                  api_key: this.api_key
+                }
+              })
+              .then(resp => {
+                resp.data.cast.slice(0, 5).forEach(item => {
+                  cast.push(item.name);
+                });
+                item.cast = cast.toString();
+                this.tvSeries.push(item); // pushing cast key into tvSeries array
+              })
 
           });
 
         });
     },
-    voteAverage: function(item) {
+
+        getTrendingMovies() {
+      axios
+        .get('https://api.themoviedb.org/3/trending/movie/week?', {
+        params: {
+          api_key: this.api_key
+        }
+      })
+        .then(resp => {
+          this.trendingMovies = resp.data.results;
+
+          
+          // pushes languages into the flags array, without duplicating them
+          this.trendingMovies.forEach(item => {
+
+            this.flags = [];
+
+            if (!this.flags.includes(item.original_language)) {
+              this.flags.push(item.original_language)
+            }
+          });
+        
+
+          // gets the firsts 5 cast names
+          this.trendingMovies.forEach(item => {
+
+            this.trendingMovies = []; // emptying array, otherwise it would duplicate trendingMovies
+
+            let cast = [];
+            axios.get(`https://api.themoviedb.org/3/movie/${item.id}/credits?`, {
+                params: {
+                  api_key: this.api_key
+                }
+              })
+              .then(resp => {
+                resp.data.cast.slice(0, 5).forEach(item => {
+                  cast.push(item.name);
+                });
+                item.cast = cast.toString();
+                this.trendingMovies.push(item); // pushing cast key into trendingMovies array
+              })
+          });
+
+        })
+
+      
+    },
+
+    getTrendingTv() {
+      axios
+        .get('https://api.themoviedb.org/3/trending/tv/week?', {
+        params: {
+          api_key: this.api_key
+        }
+      })
+        .then(resp => {
+          this.trendingTv = resp.data.results;
+
+          
+          // pushes languages into the flags array, without duplicating them
+          this.trendingTv.forEach(item => {
+
+            this.flags = [];
+
+            if (!this.flags.includes(item.original_language)) {
+              this.flags.push(item.original_language)
+            }
+          });
+        
+
+          // gets the firsts 5 cast names
+          this.trendingTv.forEach(item => {
+
+            this.trendingTv = []; // emptying array, otherwise it would duplicate trendingMovies
+
+            let cast = [];
+            axios.get(`https://api.themoviedb.org/3/movie/${item.id}/credits?`, {
+                params: {
+                  api_key: this.api_key
+                }
+              })
+              .then(resp => {
+                resp.data.cast.slice(0, 5).forEach(item => {
+                  cast.push(item.name);
+                });
+                item.cast = cast.toString();
+                this.trendingTv.push(item); // pushing cast key into trendingMovies array
+              })
+          });
+
+        })
+
+      
+    },
+
+    getGenres() {
+      axios
+        .get('https://api.themoviedb.org/3/genre/movie/list?', {
+        params: {
+          api_key: this.api_key
+        }
+      })
+        .then(resp => {
+          this.genres = resp.data.genres;
+        })
+    },
+
+    voteAverage (item) {
       return Math.round(item / 2);
     },
-    search: function() {
+
+    search() {
       if (this.selected !== '') {
         this.getMovies();
         this.getTvSeries();
       }
     },
-    toggleSearch: function() {
+
+    // toggling classes
+
+    toggleSearch () {
       this.toggledSearch = !this.toggledSearch;
     },
-    toggleMenu: function() {
+
+    toggleMenu () {
       this.toggledMenu = !this.toggledMenu;
     },
-  },
-  mounted() {
 
+    toggleTrending() {
+      this.toggledTrending = !this.toggledTrending;
+      this.toggledNoContent = !this.toggledNoContent;
+    },
+
+  },
+  
+  mounted() {
+    this.getGenres();
+    this.getTrendingMovies();
+    this.getTrendingTv();
+    
 
   }
 
